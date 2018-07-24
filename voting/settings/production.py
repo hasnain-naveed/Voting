916cf5f9d8fa2fc49abc8f1ebb70c90764ebe381
Saleem Latif <saleem_ee@hotmail.com>
2018-07-24 14:37:04 +0500

    static

--- voting/settings/production.py
+++ voting/settings/production.py
@@ -5,7 +5,7 @@ from voting.settings.base import *
 DEBUG = False
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "voting/static"),
 ]
 
 STATIC_ROOT = "/home/hasnain_naveed123/static"
